c75e369629945f96fbaf36085a6be08d77e33341
Jude <jeremiahjude07@gmail.com>
2022-09-02 09:53:51 +0530

    2nd commit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>jude</h1>
+      <h1>Hello World</h1>
+      <h2>In This World very Amazing</h2>
     </div>
   );
 }
